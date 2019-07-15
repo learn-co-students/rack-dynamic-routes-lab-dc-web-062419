af85bdd7face38e01557505039c0b4da64ddba72
claudiasolbes <cssolbes@hotmail.com>
2019-07-15 16:16:32 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,15 +5,21 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items/<ITEM NAME>"
-      Item.all.each do |item|
-        binding.pry
-      resp.write "You requested the songs"
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = Item.all.find do |item|
+        item.name == item_name
+      end
+        if item == nil
+          resp.write "Item not found"
+          resp.status = 400
+        else
+          resp.write item.price
+        end
     else
       resp.write "Route not found"
       resp.status = 404
     end
-
     resp.finish
   end
 end
